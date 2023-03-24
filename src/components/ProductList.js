e1c2d2946ab898ac67c95975cea602a7b03c6986
kevit-vasanti-vaghela <vasanti.vaghela@kevit.io>
2023-03-24 13:35:19 +0530

    pagination with infinite scrolling implemented

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState,useCallback, useEffect } from 'react'
 import { useLoaderData } from 'react-router-dom'
 import ProductItem from './ProductItem'
 import classes from './ProductList.module.css'
@@ -6,11 +6,39 @@ import InfiniteScroll from 'react-infinite-scroll-component'
 
 const ProductList = () => {
     const [searchTerm, setSearchTerm] = useState('')
+    const [products, setProducts] = useState([]);
+    const [activePage, setActivePage] = useState(1);
     const productData = useLoaderData();
     
+    
+    // fetch products data
+     const fetchProducts = useCallback(async() => {
+        const url = new URL(`https://641adba89b82ded29d438067.mockapi.io/products?page=${activePage}&limit=5`)
+        // url.searchParams.append('page', 1);
+        // url.searchParams.append('limit', 10);
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'content-type':'application/json'
+            }
+        })
+    
+        const resData = await response.json();
+        console.log('RESPONSE',response)
+        console.log('RES DATA',resData)
+        setActivePage(activePage+1);
+        setProducts([...products,...resData])
+     },[activePage,products])
+
+     useEffect(() => {
+        fetchProducts()
+     },[])
+
+     
+    
     // console.log(productData)
-    const getFilteredProducts = (searchTerm,productData) => {
-        return productData.filter((product) => 
+    const getFilteredProducts = (searchTerm,products) => {
+        return products.filter((product) => 
          product.item.toLowerCase().includes(searchTerm.toLowerCase()))
     }   
 
@@ -18,13 +46,14 @@ const ProductList = () => {
         setSearchTerm(e.target.value)
     }
 
-    const filteredProducts = getFilteredProducts(searchTerm,productData)
+    const filteredProducts = getFilteredProducts(searchTerm,products)
   return (
     <div className={classes.products}>
         <input className={classes.searchbar} type="search" onChange={onChangeHandler} value={searchTerm} placeholder='Search Products' />
         <InfiniteScroll
             dataLength={filteredProducts.length} //This is important field to render the next data
-            hasMore={true}
+            next={fetchProducts}
+            hasMore={filteredProducts.length < productData.length}
             loader={<h4>Loading...</h4>}
         >
             <ul className={classes.list}>
@@ -41,8 +70,8 @@ export default ProductList
 
 export async function loader() {
     const url = new URL('https://641adba89b82ded29d438067.mockapi.io/products');
-    url.searchParams.append('page', 1);
-    url.searchParams.append('limit', 10);
+    // url.searchParams.append('page', 1);
+    // url.searchParams.append('limit', 10);
     const response = await fetch(url, {
         method: 'GET',
         headers: {
