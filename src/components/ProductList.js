b0a72bcb4ab601efa2f5f5a53b5ea9707a04fc49
kevit-vasanti-vaghela <vasanti.vaghela@kevit.io>
2023-03-24 11:47:32 +0530

    pagination added to url

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,14 +2,13 @@ import React, { useState } from 'react'
 import { useLoaderData } from 'react-router-dom'
 import ProductItem from './ProductItem'
 import classes from './ProductList.module.css'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 const ProductList = () => {
     const [searchTerm, setSearchTerm] = useState('')
     const productData = useLoaderData();
     
-    console.log(productData)
-
-    
+    // console.log(productData)
     const getFilteredProducts = (searchTerm,productData) => {
         return productData.filter((product) => 
          product.item.toLowerCase().includes(searchTerm.toLowerCase()))
@@ -22,21 +21,29 @@ const ProductList = () => {
     const filteredProducts = getFilteredProducts(searchTerm,productData)
   return (
     <div className={classes.products}>
-        <input type="search" onChange={onChangeHandler} value={searchTerm} placeholder='Search Products' />
-        <ul className={classes.list}>
-            {filteredProducts.map((product) => (
-                <ProductItem key={product.id} product={product}/>
-            ))}
-        </ul>
-  
-        </div>
+        <input className={classes.searchbar} type="search" onChange={onChangeHandler} value={searchTerm} placeholder='Search Products' />
+        <InfiniteScroll
+            dataLength={filteredProducts.length} //This is important field to render the next data
+            hasMore={true}
+            loader={<h4>Loading...</h4>}
+        >
+            <ul className={classes.list}>
+                {filteredProducts.map((product) => (
+                    <ProductItem key={product.id} product={product}/>
+                ))}
+            </ul>
+        </InfiniteScroll>
+    </div>
   )
 }
 
 export default ProductList
 
 export async function loader() {
-    const response = await fetch('https://641adba89b82ded29d438067.mockapi.io/products', {
+    const url = new URL('https://641adba89b82ded29d438067.mockapi.io/products');
+    url.searchParams.append('page', 1);
+    url.searchParams.append('limit', 10);
+    const response = await fetch(url, {
         method: 'GET',
         headers: {
             'content-type':'application/json'
