9468171290de45959a7ee6edc74ce2fe125e4176
kevit-vasanti-vaghela <vasanti.vaghela@kevit.io>
2023-03-24 18:09:14 +0530

    total item reflected on cart badge

--- src/components/ProductNavigation.js
+++ src/components/ProductNavigation.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import classes from './ProductNavigation.module.css'
-import { NavLink } from 'react-router-dom'
-
+import { NavLink, useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 const ProductNavigation = () => {
+  const navigate = useNavigate();
+  const cartQuantity = useSelector(state=> state.cart.totalQuantity);
+  const moveToCartHandler = () => {
+    navigate('cart')
+  }
   return (
     <div className={classes['nav-div']}>
         <header className={classes.header}>
@@ -24,15 +29,10 @@ const ProductNavigation = () => {
     <nav>
         <ul className={classes.list}>
           <li>
-            <NavLink
-              to="cart"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-              end
-            >
-             Cart
-            </NavLink>
+            <button className={classes['cart-button']} onClick={moveToCartHandler}>
+              <span>My Cart</span>
+              <span className={classes.badge}>{cartQuantity}</span>
+            </button>
           </li>
           <li>
             <NavLink
